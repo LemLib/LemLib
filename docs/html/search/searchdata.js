3fe29ead9035134a3a23e36b0586f7b3199b4cc7
Exortions <exortionsmc@gmail.com>
2023-02-12 21:02:45 -0800

    first compilation of doxygen

---
+++ docs/html/search/searchdata.js
@@ -0,0 +1,18 @@
+var indexSectionsWithContent =
+{
+  0: "l",
+  1: "l"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "pages"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Pages"
+};
+
