768b61c5b16fd8d9ee1a46ec0792f62f425e852b
SizzinSeal <SizzinSeal@users.noreply.github.com>
2023-03-23 03:15:34 +0000

    deploy: 7af49c682dd20faee3e3c5a02bdd31c8ba416176

--- search/functions_d.js
+++ search/functions_d.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['trackingwheel_0',['TrackingWheel',['../classlemlib_1_1TrackingWheel.html#a24894519b426aa122aaf710c5a420b2a',1,'lemlib::TrackingWheel::TrackingWheel(pros::ADIEncoder *encoder, float diameter, float distance)'],['../classlemlib_1_1TrackingWheel.html#a7b61b9c9779ae523d5b218c0f8f13d46',1,'lemlib::TrackingWheel::TrackingWheel(pros::Rotation *encoder, float diameter, float distance)'],['../classlemlib_1_1TrackingWheel.html#a722d96ceab11756ce180f3cbf98728d8',1,'lemlib::TrackingWheel::TrackingWheel(pros::Motor_Group *motors, float diameter, float distance, float rpm)']]],
+  ['trackingwheel_0',['TrackingWheel',['../classlemlib_1_1TrackingWheel.html#ac6376015551083cc769e3afab5024fda',1,'lemlib::TrackingWheel::TrackingWheel(pros::ADIEncoder *encoder, float diameter, float distance, float gearRatio=1)'],['../classlemlib_1_1TrackingWheel.html#a08744ef3967ce157fb890a3a9b0bb4c3',1,'lemlib::TrackingWheel::TrackingWheel(pros::Rotation *encoder, float diameter, float distance, float gearRatio=1)'],['../classlemlib_1_1TrackingWheel.html#a722d96ceab11756ce180f3cbf98728d8',1,'lemlib::TrackingWheel::TrackingWheel(pros::Motor_Group *motors, float diameter, float distance, float rpm)']]],
   ['turnto_1',['turnTo',['../classlemlib_1_1Chassis.html#ab6bafad2ac9224ef669eb3a8caec7085',1,'lemlib::Chassis']]]
 ];
