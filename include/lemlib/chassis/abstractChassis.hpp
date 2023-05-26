9e8d118cf5348c56c2c2996f77342748ca897ffb
Rocky14683 <rocky10208@gmail.com>
2023-05-26 09:08:36 +0800

    add abstractChassis class

---
+++ include/lemlib/chassis/abstractChassis.hpp
@@ -0,0 +1,111 @@
+//
+// Created by Rocky Chen on 2023/5/26.
+//
+
+#pragma once
+#include "pros/motors.hpp"
+#include "pros/imu.hpp"
+#include "lemlib/chassis/trackingWheel.hpp"
+#include "lemlib/pose.hpp"
+
+namespace lemlib {
+
+    class abstractChassis {
+    public:
+    /**
+     * @brief Struct containing all the sensors used for odometry
+     *
+     * The sensors are stored in a struct so that they can be easily passed to the chassis class
+     * The variables are pointers so that they can be set to nullptr if they are not used
+     * Otherwise the chassis class would have to have a constructor for each possible combination of sensors
+     *
+     * @param vertical1 pointer to the first vertical tracking wheel
+     * @param vertical2 pointer to the second vertical tracking wheel
+     * @param horizontal1 pointer to the first horizontal tracking wheel
+     * @param horizontal2 pointer to the second horizontal tracking wheel
+     * @param imu pointer to the IMU
+     */
+        typedef struct {
+            TrackingWheel* vertical1;
+            TrackingWheel* vertical2;
+            TrackingWheel* horizontal1;
+            TrackingWheel* horizontal2;
+            pros::Imu* imu;
+        } OdomSensors_t;
+
+        /**
+         * @brief Struct containing constants for a chassis controller
+         *
+         * The constants are stored in a struct so that they can be easily passed to the chassis class
+         * Set a constant to 0 and it will be ignored
+         *
+         * @param kP proportional constant for the chassis controller
+         * @param kD derivative constant for the chassis controller
+         * @param smallError the error at which the chassis controller will switch to a slower control loop
+         * @param smallErrorTimeout the time the chassis controller will wait before switching to a slower control loop
+         * @param largeError the error at which the chassis controller will switch to a faster control loop
+         * @param largeErrorTimeout the time the chassis controller will wait before switching to a faster control loop
+         * @param slew the maximum acceleration of the chassis controller
+         */
+        typedef struct {
+            float kP;
+            float kD;
+            float smallError;
+            float smallErrorTimeout;
+            float largeError;
+            float largeErrorTimeout;
+            float slew;
+        } ChassisController_t;
+
+        /**
+         * @brief Struct containing constants for a drivetrain
+         *
+         */
+        typedef struct {
+        } Drivetrain_t;
+
+
+        abstractChassis() = delete;
+        void operator=(const abstractChassis& rhs) = delete;
+
+        template<class ChassisType> friend class chassisBuilder;
+        /**
+         * @brief Calibrate the chassis sensors
+         *
+         */
+        virtual void calibrate();
+        /**
+         * @brief Set the pose of the chassis
+         *
+         * @param x new x value
+         * @param y new y value
+         * @param theta new theta value
+         * @param radians true if theta is in radians, false if not. False by default
+         */
+        virtual void setPose(double x, double y, double theta, bool radians = false);
+        /**
+         * @brief Set the pose of the chassis
+         *
+         * @param pose the new pose
+         * @param radians whether pose theta is in radians (true) or not (false). false by default
+         */
+        virtual void setPose(Pose pose, bool radians = false);
+        /**
+         * @brief Get the pose of the chassis
+         *
+         * @param radians whether theta should be in radians (true) or degrees (false). false by default
+         * @return Pose
+         */
+        virtual Pose getPose(bool radians = false);
+
+    protected:
+
+        ChassisController_t lateralSettings;
+        ChassisController_t angularSettings;
+        ChassisController_t strafeSettings;
+        Drivetrain_t drivetrain;
+        OdomSensors_t odomSensors;
+    };
+
+} // lemlib
+
---
+++ src/lemlib/chassis/abstractChassis.cpp
@@ -0,0 +1,37 @@
+//
+// Created by Rocky Chen on 2023/5/26.
+//
+#pragma once
+#include "pros/misc.hpp"
+#include "lemlib/chassis/abstractChassis.hpp"
+#include "lemlib/chassis/odom.hpp"
+
+namespace lemlib {
+
+    /**
+     * @brief Set the Pose object
+     *
+     * @param x new x value
+     * @param y new y value
+     * @param theta new theta value
+     * @param radians true if theta is in radians, false if not. False by default
+ */
+    void abstractChassis::setPose(double x, double y, double theta, bool radians) {
+        lemlib::setPose(lemlib::Pose(x, y, theta), radians);
+    }
+    /**
+     * @brief Set the pose of the chassis
+     *
+     * @param Pose the new pose
+     * @param radians whether pose theta is in radians (true) or not (false). false by default
+     */
+    void abstractChassis::setPose(Pose pose, bool radians) { lemlib::setPose(pose, radians); }
+    /**
+     * @brief Get the pose of the chassis
+     *
+     * @param radians whether theta should be in radians (true) or degrees (false). false by default
+     * @return Pose
+     */
+    Pose abstractChassis::getPose(bool radians) { return lemlib::getPose(radians); }
+
+} // lemlib
