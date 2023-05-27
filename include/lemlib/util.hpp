cd31047322b74c77a0e9963fdf1722d69f656a34
Liam Teale <111480281+SizzinSeal@users.noreply.github.com>
2023-05-27 15:07:18 -0700

    Fix format violations

--- include/lemlib/util.hpp
+++ include/lemlib/util.hpp
@@ -85,7 +85,8 @@ double avg(std::vector<double> values);
  * @param format the format string
  * @return std::string tje formatted string
  */
-template <typename... Args> std::string format(const std::string& format, Args... args) {
+template <typename... Args>
+std::string format(const std::string& format, Args... args) {
     int size_s = std::snprintf(nullptr, 0, format.c_str(), args...) + 1; // Extra space for '\0'
     if (size_s <= 0) { throw std::runtime_error("Error during formatting."); }
     auto size = static_cast<size_t>(size_s);
