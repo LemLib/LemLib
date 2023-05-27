2eaa5e0a9a0270e582535b3d72e96978e9795dee
Rocky14683 <rocky10208@gmail.com>
2023-05-27 23:14:08 +0800

    Added HolonomicDrive class basis structure

---
+++ include/lemlib/chassis/holonomicDrive.hpp
@@ -0,0 +1,34 @@
+//
+// Created by Rocky Chen on 2023/5/26.
+//
+#pragma once
+#include "lemlib/chassis/holonomicDrive.hpp"
+#include "pros/motors.hpp"
+#include "lemlib/chassis/abstractChassis.hpp"
+
+namespace lemlib {
+
+    class HolonomicDrive : public AbstractChassis{
+        public:
+        typedef struct {
+            pros::Motor* rf;
+            pros::Motor* rb;
+            pros::Motor* lf;
+            pros::Motor* lb;
+            float trackWidth;
+            float wheelDiameter;
+            float rpm;
+        } Drivetrain_t;
+    protected:
+        HolonomicDrive() = default;
+        HolonomicDrive(HolonomicDrive::Drivetrain_t driveSetting, ChassisController_t lateralController,
+                       ChassisController_t angularController, ChassisController_t strafeController, OdomSensors_t sensors = {nullptr, nullptr, nullptr,
+                                                                                       nullptr, nullptr});
+    private:
+        HolonomicDrive::Drivetrain_t driveSetting;
+    };
+
+    typedef HolonomicDrive XDrive;
+    typedef HolonomicDrive MecanumDrive;
+
+} // lemlib
---
+++ src/lemlib/chassis/holonomicDrive.cpp
@@ -0,0 +1,17 @@
+//
+// Created by Rocky Chen on 2023/5/26.
+//
+
+#include "lemlib/chassis/holonomicDrive.hpp"
+#include "lemlib/chassis/abstractChassis.hpp"
+
+namespace lemlib {
+
+    HolonomicDrive::HolonomicDrive(HolonomicDrive::Drivetrain_t driveSetting, ChassisController_t lateralController,
+               ChassisController_t angularController, ChassisController_t strafeController, OdomSensors_t sensors):
+               AbstractChassis(lateralController, angularController,
+                                strafeController, sensors),
+               driveSetting(driveSetting){};
+
+
+} // lemlib
