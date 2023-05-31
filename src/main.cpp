6ec123a4a5fe3836d068e0b2010ebd21d9d97319
Rocky14683 <rocky10208@gmail.com>
2023-05-31 16:40:52 +0800

    Format

--- src/main.cpp
+++ src/main.cpp
@@ -5,16 +5,15 @@ using namespace lemlib;
 // drive motors
 pros::Motor lF(-3,
                pros::E_MOTOR_GEARSET_06); // left front motor. port 3, reversed
-pros::Motor
-    lM(-14, pros::E_MOTOR_GEARSET_06); // left middle motor. port 14, reversed
+pros::Motor lM(-14, pros::E_MOTOR_GEARSET_06); // left middle motor. port 14, reversed
 pros::Motor lB(-12,
                pros::E_MOTOR_GEARSET_06); // left back motor. port 12, reversed
 pros::Motor rF(19, pros::E_MOTOR_GEARSET_06); // right front motor. port 19
 pros::Motor rM(20, pros::E_MOTOR_GEARSET_06); // right middle motor. port 20
-pros::Motor rB(1, pros::E_MOTOR_GEARSET_06);  // right back motor. port 1
+pros::Motor rB(1, pros::E_MOTOR_GEARSET_06); // right back motor. port 1
 pros::Motor dirControl(1, pros::E_MOTOR_GEARSET_06);
 // motor groups
-pros::MotorGroup leftMotors({lF, lM, lB});  // left motor group
+pros::MotorGroup leftMotors({lF, lM, lB}); // left motor group
 pros::MotorGroup rightMotors({rF, rM, rB}); // right motor group
 
 // Inertial Sensor on port 6
@@ -26,30 +25,27 @@ pros::ADIEncoder verticalEnc('A', 'B', false);
 lemlib::TrackingWheel vertical(&verticalEnc, 2.75, 0);
 
 // drivetrain
-lemlib::DifferentialDrive::Drivetrain_t drivetrain{
+lemlib::DifferentialDrive::Drivetrain_t drivetrain {
     &leftMotors, &rightMotors, 10, 3.25, 360,
 };
 
-lemlib::XDrive::Drivetrain_t holoDrivetrain{
+lemlib::XDrive::Drivetrain_t holoDrivetrain {
     &rF, &rB, &lF, &lB, 10, 3.25, 360,
 };
 
 // lateral motion controller
-lemlib::DifferentialDrive::ChassisController_t lateralController{
-    10, 30, 1, 100, 3, 500, 20};
+lemlib::DifferentialDrive::ChassisController_t lateralController {10, 30, 1, 100, 3, 500, 20};
 
 // angular motion controller
-lemlib::DifferentialDrive::ChassisController_t angularController{2, 10,  1, 100,
-                                                                 3, 500, 3};
+lemlib::DifferentialDrive::ChassisController_t angularController {2, 10, 1, 100, 3, 500, 3};
 
 // sensors for odometry
-lemlib::DifferentialDrive::OdomSensors_t sensors{nullptr, nullptr, nullptr,
-                                                 nullptr, &imu};
+lemlib::DifferentialDrive::OdomSensors_t sensors {nullptr, nullptr, nullptr, nullptr, &imu};
 
-lemlib::XDrive::ChassisController_t strafeController{2, 10, 1, 100, 3, 500, 3};
+lemlib::XDrive::ChassisController_t strafeController {2, 10, 1, 100, 3, 500, 3};
 
-lemlib::SwerveDrive<lemlib::SwerveType_t::Coaxial>::Drivetrain_t swerveSetting{
-    &lF, &lB, &rF, &rB, &dirControl, &lF, &lB, &rF, 10, 3.25, 360};
+lemlib::SwerveDrive<lemlib::SwerveType_t::Coaxial>::Drivetrain_t swerveSetting {&lF, &lB, &rF, &rB,  &dirControl, &lF,
+                                                                                &lB, &rF, 10,  3.25, 360};
 
 auto diffDrive = lemlib::ChassisBuilder<lemlib::DifferentialDrive>()
                      .withSetting(drivetrain)
@@ -58,8 +54,7 @@ auto diffDrive = lemlib::ChassisBuilder<lemlib::DifferentialDrive>()
                      .withSensor(sensors)
                      .build();
 
-auto holoDrive = lemlib::ChassisBuilder<
-                     lemlib::XDrive>() // Holonomic = XDrive = MecanumDrive
+auto holoDrive = lemlib::ChassisBuilder<lemlib::XDrive>() // Holonomic = XDrive = MecanumDrive
                      .withSetting(holoDrivetrain)
                      .withLateralController(lateralController)
                      .withAngularController(angularController)
@@ -84,10 +79,9 @@ auto bruhDrive = ChassisBuilder<SwerveDrive<SwerveType_t::Coaxial>>()
  * to keep execution time for this mode under a few seconds.
  */
 void initialize() {
-  pros::lcd::initialize();
-  // calibrate sensors
-  while (true) {
-  }
+    pros::lcd::initialize();
+    // calibrate sensors
+    while (true) {}
 }
 
 /**
