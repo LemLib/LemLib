103d584fa6c751b6fd80b88dc45bdc883d1c3066
BattleCh1cken <BattleCh1cken@larkov.de>
2023-08-13 23:23:23 -0400

    Fixed curvature drive

--- src/lemlib/chassis/opcontrol.cpp
+++ src/lemlib/chassis/opcontrol.cpp
@@ -12,27 +12,30 @@ double calcDriveCurve(double input, double scale) {
     return input;
 }
 
-void Chassis::tank(int left, int right, float gain) {
-    drivetrain.leftMotors->move(calcDriveCurve(left, gain));
-    drivetrain.rightMotors->move(calcDriveCurve(right, gain));
+void Chassis::tank(int left, int right, float curveGain) {
+    drivetrain.leftMotors->move(calcDriveCurve(left, curveGain));
+    drivetrain.rightMotors->move(calcDriveCurve(right, curveGain));
 };
 
-void Chassis::arcade(int forward, int turn, float gain) {
-    int leftPower = calcDriveCurve(forward + turn, gain);
-    int rightPower = calcDriveCurve(forward - turn, gain);
+void Chassis::arcade(int forward, int turn, float curveGain) {
+    int leftPower = calcDriveCurve(forward + turn, curveGain);
+    int rightPower = calcDriveCurve(forward - turn, curveGain);
     drivetrain.leftMotors->move(leftPower);
     drivetrain.rightMotors->move(rightPower);
 };
 
-void Chassis::curvature(int forward, int curvature, float gain) {
+void Chassis::curvature(int forward, int curvature, float cureveGain) {
     // If we're not moving forwards change to arcade drive
     if (forward == 0) {
-        arcade(forward, curvature, gain);
+        arcade(forward, curvature, cureveGain);
         return;
     }
 
-    double leftPower = forward + std::abs(forward) * curvature;
-    double rightPower = forward - std::abs(forward) * curvature;
+    double leftPower = forward + (std::abs(forward) * curvature) / 127.0;
+    double rightPower = forward - (std::abs(forward) * curvature) / 127.0;
+
+    leftPower = calcDriveCurve(leftPower, cureveGain);
+    rightPower = calcDriveCurve(rightPower, cureveGain);
 
     drivetrain.leftMotors->move(leftPower);
     drivetrain.rightMotors->move(rightPower);
