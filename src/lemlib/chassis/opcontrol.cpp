79acbe657559d08057f5de5136761ad88ce49bae
BattleCh1cken <BattleCh1cken@larkov.de>
2023-08-13 22:50:18 -0400

    Added an implementation for curvature drive

--- src/lemlib/chassis/opcontrol.cpp
+++ src/lemlib/chassis/opcontrol.cpp
@@ -18,13 +18,23 @@ void Chassis::tank(int left, int right, float gain) {
 };
 
 void Chassis::arcade(int forward, int turn, float gain) {
-    int leftPower = calcDriveCurve(forward - turn, gain);
-    int rightPower = calcDriveCurve(forward + turn, gain);
+    int leftPower = calcDriveCurve(forward + turn, gain);
+    int rightPower = calcDriveCurve(forward - turn, gain);
     drivetrain.leftMotors->move(leftPower);
     drivetrain.rightMotors->move(rightPower);
 };
 
-void Chassis::curvature(int forward, int turn, float gain) {
-    // TODO: implement this
+void Chassis::curvature(int forward, int curvature, float gain) {
+    // If we're not moving forwards change to arcade drive
+    if (forward == 0) {
+        arcade(forward, curvature, gain);
+        return;
+    }
+
+    double leftPower = forward + std::abs(forward) * curvature;
+    double rightPower = forward - std::abs(forward) * curvature;
+
+    drivetrain.leftMotors->move(leftPower);
+    drivetrain.rightMotors->move(rightPower);
 };
 } // namespace lemlib
