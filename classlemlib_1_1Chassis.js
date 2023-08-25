d2e7ef5246689bbbdf1a181b8938ccee75aa0740
SizzinSeal <SizzinSeal@users.noreply.github.com>
2023-08-25 22:28:41 +0000

    deploy: b2108d970d5d3509d9f0c4fbe8f2e2db009a17f9

--- classlemlib_1_1Chassis.js
+++ classlemlib_1_1Chassis.js
@@ -4,7 +4,7 @@ var classlemlib_1_1Chassis =
     [ "arcade", "classlemlib_1_1Chassis.html#a77d28cdd5f2e0dc6e3a4014ad0668e73", null ],
     [ "calibrate", "classlemlib_1_1Chassis.html#a8b23b13453c7fceae8c5b001855f410f", null ],
     [ "curvature", "classlemlib_1_1Chassis.html#a70bd49e558b1b8d6d91fc22ac249a749", null ],
-    [ "follow", "classlemlib_1_1Chassis.html#a7b4fbf2a7a402e046c7c2781ba42af7a", null ],
+    [ "follow", "classlemlib_1_1Chassis.html#aeeda212a783c644700dd97ab80882df6", null ],
     [ "getPose", "classlemlib_1_1Chassis.html#a3f2c67a4c95ec0a235e4a467c759ee5c", null ],
     [ "moveTo", "classlemlib_1_1Chassis.html#a00edce017c843ba5080f6d7494dc52bd", null ],
     [ "setPose", "classlemlib_1_1Chassis.html#a9d272c748bcd8e42076f7bca68b4552c", null ],
