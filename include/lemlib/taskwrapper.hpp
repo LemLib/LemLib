d7debd4d0efece42bf983108f34bd95f13f63255
BattleCh1cken <BattleCh1cken@larkov.de>
2023-09-02 15:21:30 -0400

    Document the TaskWrapper class

--- include/lemlib/taskwrapper.hpp
+++ include/lemlib/taskwrapper.hpp
@@ -1,7 +1,16 @@
+/**
+ * @file include/lemlib/taskwrapper.hpp
+ * @author LemLib Team
+ * @brief A Logger for LemLib.
+ * @version 0.4.5
+ * @date 2023-09-2
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
+
 // TODO: add file header and documentation
 
-// Inspired from:
-// https://theol0403.github.io/7842B-Journal/2019-10-18/task-wrapper/
 
 #pragma once
 #include <cstdio>
@@ -11,10 +20,22 @@
 
 namespace lemlib {
 
+/**
+ * @brief An abstract class meant for long running tasks.
+ */
+
 class TaskWrapper {
     protected:
+        /**
+         * @brief The function that will be run inside the task.
+         */
         virtual void loop() = 0;
     public:
+        /**
+         * @brief The function that will be run inside the task. Starts the task.
+         * @note This will not call a while loop or any delay, so those will need to be provided inside the loop method
+         * if that behaviour is desired.
+         */
         void start_task();
     private:
         pros::Task* task {nullptr};
--- src/lemlib/taskwrapper.cpp
+++ src/lemlib/taskwrapper.cpp
@@ -1,4 +1,17 @@
-// TODO: add file header and documentation
+/**
+ * @file src/lemlib/taskwrapper.cpp
+ * @author LemLib Team
+ * @brief A Logger for LemLib.
+ * @version 0.4.5
+ * @date 2023-09-2
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
+
+// PROS requires that the addresses of callbacks passed into tasks must be known at compile time. To get around this we
+// use a lambda function, and call the function we want to run inside it. For more information look here:
+// https://theol0403.github.io/7842B-Journal/2019-10-18/task-wrapper/
 
 #include "lemlib/taskwrapper.hpp"
 
