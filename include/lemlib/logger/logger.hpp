30f0604c94a8b0f8102135b0b7ab2e75899f8d5f
AbeetoChanito <132693471+AbeetoChanito@users.noreply.github.com>
2023-10-07 11:05:40 -0400

    Format

--- include/lemlib/logger/logger.hpp
+++ include/lemlib/logger/logger.hpp
@@ -15,15 +15,15 @@ namespace lemlib {
 
 /**
  * @brief Get the info sink
- * 
- * @return std::shared_ptr<InfoSink> 
+ *
+ * @return std::shared_ptr<InfoSink>
  */
 std::shared_ptr<InfoSink> infoSink();
 
 /**
  * @brief Get the telemetry sink
- * 
- * @return std::shared_ptr<TelemetrySink> 
+ *
+ * @return std::shared_ptr<TelemetrySink>
  */
 std::shared_ptr<TelemetrySink> telemetrySink();
 } // namespace lemlib
