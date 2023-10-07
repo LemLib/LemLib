e834486f6b1dc7ee6ff32b58a5d402198f643506
AbeetoChanito <132693471+AbeetoChanito@users.noreply.github.com>
2023-10-07 09:19:58 -0400

    Cast to characters

--- src/lemlib/logger/telemetrySink.cpp
+++ src/lemlib/logger/telemetrySink.cpp
@@ -10,8 +10,8 @@ void TelemetrySink::setTelemetryId(uint8_t id) { this->id = id; }
 
 fmt::dynamic_format_arg_store<fmt::format_context> TelemetrySink::getExtraFormattingArgs(const Message& messageInfo) {
     fmt::dynamic_format_arg_store<fmt::format_context> args;
-    args.push_back(fmt::arg("id", id));
-    args.push_back(fmt::arg("tele_level", static_cast<uint8_t>(messageInfo.level)));
+    args.push_back(fmt::arg("id", static_cast<char>(id)));
+    args.push_back(fmt::arg("tele_level", static_cast<char>(messageInfo.level)));
     return args;
 }
 
