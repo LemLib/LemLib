b78ca85542a130656af815b216b135374888fc9b
AbeetoChanito <132693471+AbeetoChanito@users.noreply.github.com>
2023-10-07 11:27:29 -0400

    Add missing ctor

--- src/lemlib/logger/baseSink.cpp
+++ src/lemlib/logger/baseSink.cpp
@@ -1,6 +1,8 @@
 #include "baseSink.hpp"
 
 namespace lemlib {
+BaseSink::BaseSink(std::initializer_list<std::shared_ptr<BaseSink>> sinks) { this->sinks = sinks; }
+
 void BaseSink::setLowestLevel(Level lowestLevel) { this->lowestLevel = lowestLevel; }
 
 void BaseSink::setFormat(const std::string& logFormat) { this->logFormat = logFormat; }
