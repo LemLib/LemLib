cc7fc903b0ae6e3ee711760c23f040932cb845b9
AbeetoChanito <132693471+AbeetoChanito@users.noreply.github.com>
2023-10-07 20:08:08 -0400

    Remove unneccesary logger ID

--- include/lemlib/logger/telemetrySink.hpp
+++ include/lemlib/logger/telemetrySink.hpp
@@ -14,20 +14,6 @@ class TelemetrySink : public BaseSink {
          *
          */
         TelemetrySink();
-
-        /**
-         * @brief Set the telemetry id for logging
-         *
-         * @param id
-         */
-        void setTelemetryId(uint8_t id);
-    protected:
-        /**
-         * @brief Get the arguments for formatting
-         *
-         * @return fmt::dynamic_format_arg_store<fmt::format_context>
-         */
-        fmt::dynamic_format_arg_store<fmt::format_context> getExtraFormattingArgs(const Message& messageInfo) override;
     private:
         /**
          * @brief Log the given message
@@ -35,7 +21,5 @@ class TelemetrySink : public BaseSink {
          * @param message
          */
         void logMessage(const Message& message) override;
-
-        uint8_t id;
 };
 } // namespace lemlib
--- src/lemlib/logger/telemetrySink.cpp
+++ src/lemlib/logger/telemetrySink.cpp
@@ -4,16 +4,7 @@
 #include "stdout.hpp"
 
 namespace lemlib {
-TelemetrySink::TelemetrySink() { setFormat("TELE_BEGIN{id}{tele_level}{message}TELE_END"); }
-
-void TelemetrySink::setTelemetryId(uint8_t id) { this->id = id; }
-
-fmt::dynamic_format_arg_store<fmt::format_context> TelemetrySink::getExtraFormattingArgs(const Message& messageInfo) {
-    fmt::dynamic_format_arg_store<fmt::format_context> args;
-    args.push_back(fmt::arg("id", static_cast<char>(id)));
-    args.push_back(fmt::arg("tele_level", static_cast<char>(messageInfo.level)));
-    return args;
-}
+TelemetrySink::TelemetrySink() { setFormat("TELE_{level}:{message}TELE_END"); }
 
 void TelemetrySink::logMessage(const Message& message) { Stdout::print("\033[s{}\033[u\033[0J", message.message); }
 } // namespace lemlib
