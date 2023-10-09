59e8ab4c571f543318b8c7b175dbbc0817d377d9
Felix Hass <75806385+BattleCh1cken@users.noreply.github.com>
2023-10-08 22:48:04 -0400

    Clean up includes in stdout.cpp
    Co-authored-by: Liam Teale <111480281+SizzinSeal@users.noreply.github.com>

--- src/lemlib/logger/stdout.cpp
+++ src/lemlib/logger/stdout.cpp
@@ -1,7 +1,7 @@
-#include "stdout.hpp"
-
 #include <iostream>
 #include <ostream>
+
+#include "lemlib/logger/stdout.hpp"
 
 namespace lemlib {
 Stdout::Stdout() : Buffer([](const std::string& text) { std::cout << text << std::flush; }) { setRate(50); }
