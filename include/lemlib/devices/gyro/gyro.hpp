6970f3efbea7eaa1055adb6428864f2f6dabf132
Liam Teale <liamteale6@gmail.com>
2023-10-16 09:21:16 -0700

    Start work on Gyro implementation

--- include/lemlib/devices/gyro/gyro.hpp
+++ include/lemlib/devices/gyro/gyro.hpp
@@ -1,7 +1,5 @@
 #pragma once
 
-#include <numeric>
-
 namespace lemlib {
 class Gyro {
     public:
@@ -10,30 +8,30 @@ class Gyro {
          *
          * @param blocking whether the function should block until calibration is complete
          * @return true calibration failed
-         * @return false calibration succeeded, or blocking is set to false
+         * @return false calibration succeeded
          */
-        virtual bool calibrate(bool blocking = false);
+        virtual bool calibrate(bool blocking = false) = 0;
         /**
          * @brief Get wether the gyro is calibrating or not
          *
          * @return true Gyro is calibrating
          * @return false Gyro is not calibrating
          */
-        virtual bool isCalibrating();
+        virtual bool isCalibrating() = 0;
         /**
          * @brief Get wether the gyro is calibrated or not
          *
          * @return true the gyro is calibrated
          * @return false the gyro is not calibrated
          */
-        virtual bool isCalibrated();
+        virtual bool isCalibrated() = 0;
         /**
          * @brief Get whether the gyro is connected or not
          *
          * @return true the gyro is connected
          * @return false the gyro is not connected
          */
-        virtual bool isConnected();
+        virtual bool isConnected() = 0;
         /**
          * @brief Get the heading of the gyro
          *
@@ -41,7 +39,7 @@ class Gyro {
          *
          * @return float heading, in radians, locked from 0-2pi
          */
-        virtual float getHeading();
+        virtual float getHeading() = 0;
         /**
          * @brief Get the orientation of the gyro
          *
@@ -49,7 +47,7 @@ class Gyro {
          *
          * @return float orientation, in radians
          */
-        virtual float getOrientation();
+        virtual float getOrientation() = 0;
         /**
          * @brief Set the orientation of the gyro
          *
@@ -57,6 +55,6 @@ class Gyro {
          *
          * @param orientation orientation, in radians
          */
-        virtual void setOrientation(float orientation);
+        virtual void setOrientation(float orientation) = 0;
 };
 } // namespace lemlib
