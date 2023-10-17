5ba116bdc9b3d35046ec4d23ff33cbf664a8747c
Liam Teale <liamteale6@gmail.com>
2023-10-17 11:48:57 -0700

    Implement Imu

--- include/lemlib/devices/gyro/imu.hpp
+++ include/lemlib/devices/gyro/imu.hpp
@@ -26,52 +26,52 @@ class Imu : public Gyro {
          * @return true calibration failed
          * @return false calibration succeeded
          */
-        bool calibrate(bool blocking = false);
+        bool calibrate(bool blocking = false) override;
         /**
          * @brief Get wether the IMU is calibrating or not
          *
          * @return true IMU is calibrating
          * @return false IMU is not calibrating
          */
-        bool isCalibrating();
+        bool isCalibrating() override;
         /**
          * @brief Get wether the IMU is calibrated or not
          *
          * @return true the IMU is calibrated
          * @return false the IMU is not calibrated
          */
-        bool isCalibrated();
+        bool isCalibrated() override;
         /**
          * @brief Get whether the IMU is connected or not
          *
          * @return true the IMU is connected
          * @return false the IMU is not connected
          */
-        bool isConnected();
+        bool isConnected() override;
         /**
-         * @brief Get the heading of the gyro
+         * @brief Get the heading of the IMU
          *
          * @note 0 is in the positive x direction, and heading increases counterclockwise
          *
-         * @return float heading, in radians, locked from 0-2pi
+         * @return float heading, in radians, locked from -pi to pi
          */
-        float getHeading();
+        float getHeading() override;
         /**
-         * @brief Get the orientation of the gyro
+         * @brief Get the rotation of the IMU
          *
          * @note 0 is in the positive x direction, and heading increases counterclockwise
          *
-         * @return float orientation, in radians
+         * @return float rotation, in radians
          */
-        float getOrientation();
+        float getRotation() override;
         /**
-         * @brief Set the orientation of the gyro
+         * @brief Set the rotation of the IMU
          *
          * @brief 0 is in the positive x direction, and heading increases counterclockwise
          *
-         * @param orientation orientation, in radians
+         * @param rotation, rotation in radians
          */
-        void setOrientation(float orientation);
+        void setRotation(float rotation) override;
     private:
         pros::Imu imu;
 };
