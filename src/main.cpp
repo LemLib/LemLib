c6869dee602a47f95e3aa1bc7357e79f461e49c5
Liam Teale <liamteale6@gmail.com>
2023-10-28 14:23:35 -0700

    improve main.cpp documentation

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,9 @@
 #include "lemlib/chassis/chassis.hpp"
 #include "lemlib/logger/stdout.hpp"
 
-// drive motor groups
+// left drive motors on ports 8, 20, 18. Motors on ports 8 and 20 are reversed
 auto leftDrive = lemlib::makeMotorGroup({-8, -20, 19}, pros::v5::MotorGears::blue);
+// right drive motors on ports 2, 11, 13. Motor on port 13 is reversed
 auto rightDrive = lemlib::makeMotorGroup({2, 11, -13}, pros::v5::MotorGears::blue);
 
 // Inertial Sensor on port 12
@@ -14,7 +15,14 @@ pros::Imu imu(12);
 lemlib::TrackingWheel vertical(4, lemlib::Omniwheel::NEW_275, -3.7);
 
 // drivetrain
-lemlib::Drivetrain_t drivetrain {leftDrive, rightDrive, 10, lemlib::Omniwheel::NEW_4, 300, 8};
+lemlib::Drivetrain_t drivetrain {
+    leftDrive, // left drivetrain motors
+    rightDrive, // right drivetrain motors
+    10, // track width of robot (distance from left wheels to distance of right)
+    lemlib::Omniwheel::NEW_4, // wheel diameter
+    300, // rpm of the drivetrain
+    8 // chase power. Higher values result in sharper turns
+};
 
 // lateral motion controller
 lemlib::ChassisController_t lateralController {
@@ -39,9 +47,20 @@ lemlib::ChassisController_t angularController {
 };
 
 // sensors for odometry
-lemlib::OdomSensors_t sensors {&vertical, nullptr, nullptr, nullptr, &imu};
+lemlib::OdomSensors_t sensors {
+    &vertical, // vertical tracking wheel
+    nullptr, // we don't have a second vertical tracking wheel
+    nullptr, // we don't have a horizontal tracking wheel
+    nullptr, // we don't have a horizontal tracking wheel
+    &imu // inertial sensor (AKA imu)
+};
 
-lemlib::Chassis chassis(drivetrain, lateralController, angularController, sensors);
+// chassis
+lemlib::Chassis chassis(drivetrain, // drivetrain struct
+                        lateralController, // forwards/backwards PID struct
+                        angularController, // turning PID struct
+                        sensors // sensors struct
+);
 
 /**
  * Runs initialization code. This occurs as soon as the program is started.
@@ -50,7 +69,7 @@ lemlib::Chassis chassis(drivetrain, lateralController, angularController, sensor
  * to keep execution time for this mode under a few seconds.
  */
 void initialize() {
-    pros::lcd::initialize();
+    pros::lcd::initialize(); // initialize brain screen
     chassis.initialize(); // calibrate sensors
 
     // the default rate is 50. however, if you need to change the rate, you
@@ -61,60 +80,69 @@ void initialize() {
     // for more information on how the formatting for the loggers
     // works, refer to the fmtlib docs
 
-    // print odom values to the brain
+    // thread to for brain screen and position logging
     pros::Task screenTask([=]() {
         while (true) {
-            pros::lcd::print(0, "X: %f", chassis.getPose().x);
-            pros::lcd::print(1, "Y: %f", chassis.getPose().y);
-            pros::lcd::print(2, "Theta: %f", chassis.getPose().theta);
+            // print to the brain screen
+            pros::lcd::print(0, "X: %f", chassis.getPose().x); // x
+            pros::lcd::print(1, "Y: %f", chassis.getPose().y); // y
+            pros::lcd::print(2, "Theta: %f", chassis.getPose().theta); // heading
+            // log position telemetry
             lemlib::telemetrySink()->info("Chassis pose: {}", chassis.getPose());
+            // delay to save resources
             pros::delay(50);
         }
     });
 }
 
 /**
- * Runs while the robot is in the disabled state of Field Management System or
- * the VEX Competition Switch, following either autonomous or opcontrol. When
- * the robot is enabled, this task will exit.
+ * Runs while the robot is disabled
  */
 void disabled() {}
 
 /**
- * Runs after initialize(), and before autonomous when connected to the Field
- * Management System or the VEX Competition Switch. This is intended for
- * competition-specific initialization routines, such as an autonomous selector
- * on the LCD.
- *
- * This task will exit when the robot is enabled and autonomous or opcontrol
- * starts.
+ * runs after initialize if the robot is connected to field control
  */
 void competition_initialize() {}
 
-/**
- * Runs the user autonomous code. This function will be started in its own task
- * with the default priority and stack size whenever the robot is enabled via
- * the Field Management System or the VEX Competition Switch in the autonomous
- * mode. Alternatively, this function may be called in initialize or opcontrol
- * for non-competition testing purposes.
- *
- * If the robot is disabled or communications is lost, the autonomous task
- * will be stopped. Re-enabling the robot will restart the task, not re-start it
- * from where it left off.
- */
-void autonomous() {}
+// get a path used for pure pursuit
+// this needs to be put outside a function
+ASSET(path_txt); // '.' replaced with "_" to make c++ happy
 
 /**
- * Runs the operator control code. This function will be started in its own task
- * with the default priority and stack size whenever the robot is enabled via
- * the Field Management System or the VEX Competition Switch in the operator
- * control mode.
+ * Runs during auto
  *
- * If no competition control is connected, this function will run immediately
- * following initialize().
- *
- * If the robot is disabled or communications is lost, the
- * operator control task will be stopped. Re-enabling the robot will restart the
- * task, not resume it from where it left off.
+ * This is an example autonomous routine which demonstrates a lot of the features LemLib has to offer
  */
-void opcontrol() { chassis.moveTo(20, 15, 90, 4000); }
+void autonomous() {
+    // example movement: Move to x: 20 and y:15, and face heading 90. Timeout set to 4000 ms
+    chassis.moveTo(20, 15, 90, 4000);
+    // example movement: Turn to face the point x:45, y:-45. Timeout set to 1000
+    // dont turn faster than 60 (out of a maximum of 127)
+    chassis.turnToPose(45, -45, 1000, true, 60);
+    // example movement: Follow the path in path.txt. Lookahead at 15, Timeout set to 4000
+    // following the path with the back of the robot (forwards = false)
+    // see line 110 to see how to define a path
+    chassis.follow(path_txt, 15, 4000, false);
+    // wait until the chassis has travelled 10 inches. Otherwise the code directly after
+    // the movement will run immediately
+    // Unless its another movement, in which case it will wait
+    chassis.waitUntil(10);
+    pros::lcd::print(4, "Travelled 10 inches during pure pursuit!");
+    // wait until the movement is done
+    chassis.waitUntilDone();
+    pros::lcd::print(5, "pure pursuit finished!");
+}
+
+/**
+ * Runs in driver control
+ */
+void opcontrol() {
+    // controller
+    // loop to continuously update motors
+    while (true) {
+        // get joystick positions
+        // delay to save resources
+        pros::delay(10);
+    }
+}
