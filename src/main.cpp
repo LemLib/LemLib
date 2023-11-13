84c5957742961b324031a6b5878cb5e56809cc8c
Liam Teale <liamteale6@gmail.com>
2023-11-13 03:40:03 -0600

    Fix build errors

--- src/main.cpp
+++ src/main.cpp
@@ -23,9 +23,9 @@ pros::MotorGroup rightMotors({rF, rM, rB}); // right motor group
 pros::Imu imu(12);
 
 // tracking wheels
-pros::Rotation verticalEnc(4);
+pros::Rotation horizontalEnc(4);
 // horizontal tracking wheel. 2.75" diameter, 3.7" offset, back of the robot
-lemlib::TrackingWheel vertical(&verticalEnc, lemlib::Omniwheel::NEW_275, -3.7);
+lemlib::TrackingWheel horizontal(&horizontalEnc, lemlib::Omniwheel::NEW_275, -3.7);
 
 // drivetrain
 lemlib::Drivetrain_t drivetrain {&leftMotors, &rightMotors, 10, lemlib::Omniwheel::NEW_4, 300, 8};
