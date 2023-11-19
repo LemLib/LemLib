d029be3e6b10311b5c5d1273f9ef85c5ab0e56ec
Andrew Curtis <80860310+meisZWFLZ@users.noreply.github.com>
2023-11-19 21:10:27 +0000

    Make appropriate pose methods const

--- include/lemlib/pose.hpp
+++ include/lemlib/pose.hpp
@@ -36,7 +36,7 @@ class Pose {
          * @param other other pose
          * @return Pose
          */
-        Pose operator+(const Pose& other);
+        Pose operator+(const Pose& other) const;
         /**
          * @brief Set the value of this pose to its value + the value of another pose
          *
@@ -49,7 +49,7 @@ class Pose {
          * @param other other pose
          * @return Pose
          */
-        Pose operator-(const Pose& other);
+        Pose operator-(const Pose& other) const;
         /**
          * @brief Set the value of this pose to its value - the value of another pose
          *
@@ -62,35 +62,35 @@ class Pose {
          * @param other other pose
          * @return Pose
          */
-        float operator*(const Pose& other);
+        float operator*(const Pose& other) const;
         /**
          * @brief Multiply a pose by a float
          *
          * @param other float
          * @return Pose
          */
-        Pose operator*(const float& other);
+        Pose operator*(const float& other) const;
         /**
          * @brief Divide a pose by a float
          *
          * @param other float
          * @return Pose
          */
-        Pose operator/(const float& other);
+        Pose operator/(const float& other) const;
         /**
          * @brief Check if two poses are equal
          *
          * @param other the other pose
          * @return bool
          */
-        bool operator==(const Pose& other);
+        bool operator==(const Pose& other) const;
         /**
          * @brief Check if two poses are not equal
          *
          * @param other the other pose
          * @return bool
          */
-        bool operator!=(const Pose& other);
+        bool operator!=(const Pose& other) const;
         /**
          * @brief Linearly interpolate between two poses
          *
@@ -98,21 +98,21 @@ class Pose {
          * @param t t value
          * @return Pose
          */
-        Pose lerp(Pose other, float t);
+        Pose lerp(const Pose other, float t) const;
         /**
          * @brief Get the distance between two poses
          *
          * @param other the other pose
          * @return float
          */
-        float distance(Pose other);
+        float distance(const Pose other) const;
         /**
          * @brief Get the angle between two poses
          *
          * @param other the other pose
          * @return float in radians
          */
-        float angle(Pose other);
+        float angle(const Pose other) const;
         /**
          * @brief Rotate a pose by an angle
          *
@@ -121,21 +121,7 @@ class Pose {
          * @param angle angle in radians
          * @return Pose
          */
-        Pose rotate(float angle);
-        /**
-         * @brief Check that x, y, and theta are equal between two poses
-         *
-         * @param other the other pose
-         * @return bool
-         */
-        bool operator==(const Pose& other) const;
-        /**
-         * @brief Check that x, y, and theta are NOT equal between two poses
-         *
-         * @param other the other pose
-         * @return bool
-         */
-        bool operator!=(const Pose& other) const;
+        Pose rotate(float angle) const;
 };
 
 /**
