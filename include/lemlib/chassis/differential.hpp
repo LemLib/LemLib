d809139eae4e3470571edbeee8e1ed4fdd8a3660
Liam Teale <liamteale6@gmail.com>
2023-11-19 15:30:17 -0800

    fix build errors

--- include/lemlib/chassis/differential.hpp
+++ include/lemlib/chassis/differential.hpp
@@ -36,7 +36,26 @@ namespace lemlib {
  * @param horizontal2 pointer to the second horizontal tracking wheel
  * @param imu pointer to the IMU
  */
-typedef struct {
+struct OdomSensors {
+        /**
+         * The sensors are stored in a struct so that they can be easily passed to the chassis class
+         * The variables are pointers so that they can be set to nullptr if they are not used
+         * Otherwise the chassis class would have to have a constructor for each possible combination of sensors
+         *
+         * @param vertical1 pointer to the first vertical tracking wheel
+         * @param vertical2 pointer to the second vertical tracking wheel
+         * @param horizontal1 pointer to the first horizontal tracking wheel
+         * @param horizontal2 pointer to the second horizontal tracking wheel
+         * @param imu pointer to the IMU
+         */
+        OdomSensors(TrackingWheel* vertical1, TrackingWheel* vertical2, TrackingWheel* horizontal1,
+                    TrackingWheel* horizontal2, pros::Imu* imu)
+            : vertical1(vertical1),
+              vertical2(vertical2),
+              horizontal1(horizontal1),
+              horizontal2(horizontal2),
+              imu(imu) {}
+
         TrackingWheel* vertical1;
         TrackingWheel* vertical2;
         TrackingWheel* horizontal1;
@@ -58,7 +77,29 @@ typedef struct {
  * @param largeErrorTimeout the time the chassis controller will wait before switching to a faster control loop
  * @param slew the maximum acceleration of the chassis controller
  */
-typedef struct {
+struct ControllerSettings {
+        /**
+         * The constants are stored in a struct so that they can be easily passed to the chassis class
+         * Set a constant to 0 and it will be ignored
+         *
+         * @param kP proportional constant for the chassis controller
+         * @param kD derivative constant for the chassis controller
+         * @param smallError the error at which the chassis controller will switch to a slower control loop
+         * @param smallErrorTimeout the time the chassis controller will wait before switching to a slower control loop
+         * @param largeError the error at which the chassis controller will switch to a faster control loop
+         * @param largeErrorTimeout the time the chassis controller will wait before switching to a faster control loop
+         * @param slew the maximum acceleration of the chassis controller
+         */
+        ControllerSettings(float kP, float kD, float smallError, float smallErrorTimeout, float largeError,
+                           float largeErrorTimeout, float slew)
+            : kP(kP),
+              kD(kD),
+              smallError(smallError),
+              smallErrorTimeout(smallErrorTimeout),
+              largeError(largeError),
+              largeErrorTimeout(largeErrorTimeout),
+              slew(slew) {}
+
         float kP;
         float kD;
         float smallError;
@@ -81,7 +122,27 @@ typedef struct {
  * @param rpm the rpm of the wheels
  * @param chasePower higher values make the robot move faster but causes more overshoot on turns
  */
-typedef struct {
+struct Drivetrain {
+        /**
+         * The constants are stored in a struct so that they can be easily passed to the chassis class
+         * Set a constant to 0 and it will be ignored
+         *
+         * @param leftMotors shared pointer to the left motors
+         * @param rightMotors shared pointer to the right motors
+         * @param trackWidth the track width of the robot
+         * @param wheelDiameter the diameter of the wheel used on the drivetrain
+         * @param rpm the rpm of the wheels
+         * @param chasePower higher values make the robot move faster but causes more overshoot on turns
+         */
+        Drivetrain(std::shared_ptr<pros::MotorGroup> leftMotors, std::shared_ptr<pros::MotorGroup> rightMotors,
+                   float trackWidth, float wheelDiameter, float rpm, float chasePower)
+            : leftMotors(leftMotors),
+              rightMotors(rightMotors),
+              trackWidth(trackWidth),
+              wheelDiameter(wheelDiameter),
+              rpm(rpm),
+              chasePower(chasePower) {}
+
         std::shared_ptr<pros::MotorGroup> leftMotors;
         std::shared_ptr<pros::MotorGroup> rightMotors;
         float trackWidth;
