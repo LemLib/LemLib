c5599933cef2a1d82288a3b7d13b2b55cc424a6d
Raptorly1 <95979895+Raptorly1@users.noreply.github.com>
2023-11-27 16:30:50 -0800

    Update tank format differential.cpp

--- src/lemlib/chassis/differential.cpp
+++ src/lemlib/chassis/differential.cpp
@@ -270,7 +270,8 @@ void Differential::arcade(int throttle, int turn, float linearCurveGain, float t
  * @param curveGain the scale inputted into the drive curve function. If you are using the default drive
  * curve, refer to the `defaultDriveCurve` documentation.
  */
-void Differential::tank(int left, int right, float leftCurveGain, float rightCurveGain, const DriveCurveFunction_t& driveCurve) {
+void Differential::tank(int left, int right, float leftCurveGain, float rightCurveGain,
+                        const DriveCurveFunction_t& driveCurve) {
     this->drivetrain->leftMotors->move(driveCurve(left, leftCurveGain));
     this->drivetrain->rightMotors->move(driveCurve(right, rightCurveGain));
 }
