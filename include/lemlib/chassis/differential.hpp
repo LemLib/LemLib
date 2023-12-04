34f8dcdea92f3614588e30afef2b2c4fc5443144
Raptorly1 <95979895+Raptorly1@users.noreply.github.com>
2023-12-03 16:27:17 -0800

    Update differential.hpp

--- include/lemlib/chassis/differential.hpp
+++ include/lemlib/chassis/differential.hpp
@@ -273,7 +273,8 @@ class Differential : public Chassis {
          * of 0 disables the curve entirely.
          */
         void tank(int left, int right, float leftCurveGain = 0.0, float rightCurveGain = 0.0,
-                  const DriveCurveFunction_t& leftCurve = defaultDriveCurve, const DriveCurveFunction_t& rightCurve = defaultDriveCurve);
+                  const DriveCurveFunction_t& leftCurve = defaultDriveCurve,
+                  const DriveCurveFunction_t& rightCurve = defaultDriveCurve);
 
         /**
          * @brief Control the robot during the driver using the arcade drive control scheme. In this control scheme one
@@ -285,7 +286,8 @@ class Differential : public Chassis {
          * curve, refer to the `defaultDriveCurve` documentation.
          */
         void arcade(int throttle, int turn, float linearCurveGain = 0.0, float turnCurveGain = 0.0,
-                    const DriveCurveFunction_t& driveCurve = defaultDriveCurve, const DriveCurveFunction_t& turnCurve = defaultDriveCurve);
+                  const DriveCurveFunction_t& leftCurve = defaultDriveCurve,
+                  const DriveCurveFunction_t& rightCurve = defaultDriveCurve);
         /**
          * @brief Control the robot during the driver using the curvature drive control scheme. This control scheme is
          * very similar to arcade drive, except the second joystick axis controls the radius of the curve that the
@@ -297,7 +299,8 @@ class Differential : public Chassis {
          * curve, refer to the `defaultDriveCurve` documentation.
          */
         void curvature(int throttle, int turn, float linearCurveGain = 0.0, float turnCurveGain = 0.0,
-                       const DriveCurveFunction_t& driveCurve = defaultDriveCurve, const DriveCurveFunction_t& turnCurve = defaultDriveCurve);
+                       const DriveCurveFunction_t& driveCurve = defaultDriveCurve,
+                       const DriveCurveFunction_t& turnCurve = defaultDriveCurve);
     private:
         /**
          * @brief Chassis update function. Updates chassis motion and odometry
