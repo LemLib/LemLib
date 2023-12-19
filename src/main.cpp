bffa638128114a595cdfadb1766e06fbf5b6a24e
Liam Teale <liamteale6@gmail.com>
2023-12-19 07:01:48 -0800

    update example project

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,28 @@
 #include "main.h"
 #include "lemlib/api.hpp"
-#include "lemlib/logger/stdout.hpp"
-#include "pros/misc.h"
 
 // controller
 pros::Controller controller(pros::E_CONTROLLER_MASTER);
 
 // drive motors
-pros::Motor lF(-8, pros::E_MOTOR_GEARSET_06); // left front motor. port 8, reversed
-pros::Motor lM(-20, pros::E_MOTOR_GEARSET_06); // left middle motor. port 20, reversed
-pros::Motor lB(19, pros::E_MOTOR_GEARSET_06); // left back motor. port 19
-pros::Motor rF(2, pros::E_MOTOR_GEARSET_06); // right front motor. port 2
-pros::Motor rM(11, pros::E_MOTOR_GEARSET_06); // right middle motor. port 11
-pros::Motor rB(-13, pros::E_MOTOR_GEARSET_06); // right back motor. port 13, reversed
+pros::Motor lF(-12, pros::E_MOTOR_GEARSET_06); // left front motor. port 12, reversed
+pros::Motor lM(-11, pros::E_MOTOR_GEARSET_06); // left middle motor. port 11, reversed
+pros::Motor lB(-1, pros::E_MOTOR_GEARSET_06); // left back motor. port 1, reversed
+pros::Motor rF(19, pros::E_MOTOR_GEARSET_06); // right front motor. port 2
+pros::Motor rM(20, pros::E_MOTOR_GEARSET_06); // right middle motor. port 11
+pros::Motor rB(9, pros::E_MOTOR_GEARSET_06); // right back motor. port 13
 
 // motor groups
 pros::MotorGroup leftMotors({lF, lM, lB}); // left motor group
 pros::MotorGroup rightMotors({rF, rM, rB}); // right motor group
 
-// Inertial Sensor on port 11
-pros::Imu imu(12);
+// Inertial Sensor on port 2
+pros::Imu imu(2);
 
 // tracking wheels
-pros::Rotation horizontalEnc(4);
-// horizontal tracking wheel. 2.75" diameter, 3.7" offset, back of the robot
+// horizontal tracking wheel encoder. Rotation sensor, port 15, reversed (negative signs don't work due to a pros bug)
+pros::Rotation horizontalEnc(15, true);
+// horizontal tracking wheel. 2.75" diameter, 3.7" offset, back of the robot (negative)
 lemlib::TrackingWheel horizontal(&horizontalEnc, lemlib::Omniwheel::NEW_275, -3.7);
 
 // drivetrain settings
@@ -37,7 +36,9 @@ lemlib::Drivetrain drivetrain(&leftMotors, // left motor group
 
 // lateral motion controller
 lemlib::ControllerSettings linearController(10, // proportional gain (kP)
-                                            30, // derivative gain (kD)
+                                            0, // integral gain (kI)
+                                            3, // derivative gain (kD)
+                                            3, // anti windup
                                             1, // small error range, in inches
                                             100, // small error range timeout, in milliseconds
                                             3, // large error range, in inches
@@ -47,18 +48,20 @@ lemlib::ControllerSettings linearController(10, // proportional gain (kP)
 
 // angular motion controller
 lemlib::ControllerSettings angularController(2, // proportional gain (kP)
+                                             0, // integral gain (kI)
                                              10, // derivative gain (kD)
+                                             3, // anti windup
                                              1, // small error range, in degrees
                                              100, // small error range timeout, in milliseconds
                                              3, // large error range, in degrees
                                              500, // large error range timeout, in milliseconds
-                                             20 // maximum acceleration (slew)
+                                             0 // maximum acceleration (slew)
 );
 
 // sensors for odometry
-// note that in this example we use internal motor encoders, so we don't pass vertical tracking wheels
-lemlib::OdomSensors sensors(nullptr, // vertical tracking wheel 1, set to nullptr as we don't have one
-                            nullptr, // vertical tracking wheel 2, set to nullptr as we don't have one
+// note that in this example we use internal motor encoders (IMEs), so we don't pass vertical tracking wheels
+lemlib::OdomSensors sensors(nullptr, // vertical tracking wheel 1, set to null
+                            nullptr, // vertical tracking wheel 2, set to nullptr as we are using IMEs
                             &horizontal, // horizontal tracking wheel 1
                             nullptr, // horizontal tracking wheel 2, set to nullptr as we don't have a second one
                             &imu // inertial sensor
