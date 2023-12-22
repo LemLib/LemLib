6e65e69647b7ab0d20de9786bf818d92aa35ef97
Liam Teale <liamteale6@gmail.com>
2023-12-21 19:53:25 -0800

    fix distTravelled

--- src/lemlib/chassis/chassis.cpp
+++ src/lemlib/chassis/chassis.cpp
@@ -299,7 +299,7 @@ void lemlib::Chassis::moveToPose(float x, float y, float theta, int timeout, Mov
         const Pose pose = getPose(true, true);
 
         // update distance travelled
-        distTravelled = pose.distance(lastPose);
+        distTravelled += pose.distance(lastPose);
         lastPose = pose;
 
         // calculate distance to the target point
@@ -439,7 +439,7 @@ void lemlib::Chassis::moveToPoint(float x, float y, int timeout, bool forwards, 
         const Pose pose = getPose(true, true);
 
         // update distance travelled
-        distTravelled = pose.distance(lastPose);
+        distTravelled += pose.distance(lastPose);
         lastPose = pose;
 
         // calculate distance to the target point
