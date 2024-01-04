fc1ef55963fce51157473b690b63ed8741f2234a
N Cheng <74994367+Miniongolf@users.noreply.github.com>
2024-01-03 19:43:36 -0500

    Revert "Turn to heading() clang format"

--- include/lemlib/api.hpp
+++ include/lemlib/api.hpp
@@ -1,8 +1,7 @@
 /**
  * @file include/lemlib/api.hpp
  * @author LemLib Team
- * @brief LemLib API header file. Include this in your source files to use the
- * library.
+ * @brief LemLib API header file. Include this in your source files to use the library.
  * @version 0.4.5
  * @date 2023-01-27
  *
@@ -11,11 +10,14 @@
  */
 #pragma once
 
+#include "lemlib/util.hpp"
 #include "lemlib/pid.hpp"
 #include "lemlib/pose.hpp"
-#include "lemlib/util.hpp"
 
-#include "lemlib/chassis/chassis.hpp"
 #include "lemlib/chassis/trackingWheel.hpp"
+#include "lemlib/chassis/chassis.hpp"
+
+#include "lemlib/pto/pneumatics.hpp"
+#include "lemlib/pto/pto.hpp"
 
 #include "lemlib/logger/logger.hpp"
