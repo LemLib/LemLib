bd2f42faffcf1628753ebe102daa291c7a818565
N Cheng <nicholas.cheng@ucc.on.ca>
2024-01-03 19:12:09 -0500

    Remove all references to PTO, update main.cpp

--- include/lemlib/api.hpp
+++ include/lemlib/api.hpp
@@ -17,7 +17,4 @@
 #include "lemlib/chassis/trackingWheel.hpp"
 #include "lemlib/chassis/chassis.hpp"
 
-#include "lemlib/pto/pneumatics.hpp"
-#include "lemlib/pto/pto.hpp"
-
 #include "lemlib/logger/logger.hpp"
