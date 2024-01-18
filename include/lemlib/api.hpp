9694d094f76020962693e60c465384cb8073f159
N Cheng <nicholas.cheng@ucc.on.ca>
2024-01-18 11:40:35 -0500

    Remove random `api.hpp` newlines

--- include/lemlib/api.hpp
+++ include/lemlib/api.hpp
@@ -3,8 +3,6 @@
 #include "lemlib/pid.hpp"
 #include "lemlib/pose.hpp"
 #include "lemlib/util.hpp"
-
 #include "lemlib/chassis/chassis.hpp"
 #include "lemlib/chassis/trackingWheel.hpp"
-
 #include "lemlib/logger/logger.hpp"
