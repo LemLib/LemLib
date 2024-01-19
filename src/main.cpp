90deeb7553adf7cee238db70359e53cf4f8fb5ae
Liam Teale <liamteale6@gmail.com>
2024-01-18 16:05:10 -0800

    fix compilation errors

--- src/main.cpp
+++ src/main.cpp
@@ -133,7 +133,7 @@ void autonomous() {
     chassis.cancelMotion();
     // example movement: Turn to face the point x:45, y:-45. Timeout set to 1000
     // dont turn faster than 60 (out of a maximum of 127)
-    chassis.turnTo(45, -45, 1000, true, 60);
+    chassis.turnTo(45, -45, 1000, {.maxSpeed = 60});
     // example movement: Follow the path in path.txt. Lookahead at 15, Timeout set to 4000
     // following the path with the back of the robot (forwards = false)
     // see line 116 to see how to define a path
