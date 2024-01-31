7435e6cbdfa7b745ec0dceb7ab60d2967cbb359b
DancingRicardo <99092565+DancingRicardo@users.noreply.github.com>
2024-01-31 18:01:38 -0500

    Event Handling Implemented

--- include/lemlib/eventhandler/eventhandler.hpp
+++ include/lemlib/eventhandler/eventhandler.hpp
@@ -1,13 +1,20 @@
 
 #include "lemlib/logger/logger.hpp"
+#include "lemlib/eventhandler/event.hpp"
 
 namespace lemlib {
 
-class EventHandler {
+template <class T> class EventHandler {
     protected:
+        std::vector<Event<T>> eventVector;
+        std::vector<bool> eventStates;
     public:
-        EventHandler();
+        EventHandler(std::vector<Event<T>> eventVector);
         ~EventHandler();
+
+        std::vector<bool>& getCurrentEvents();
+
+        void startAsyncTask();
 };
 
 } // namespace lemlib
