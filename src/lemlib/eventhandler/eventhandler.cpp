ed2ee3661e43c1ce943fdc9bd8deba1afbe98489
DancingRicardo <99092565+DancingRicardo@users.noreply.github.com>
2024-01-31 18:01:38 -0500

    Event Handling Implemented

---
+++ src/lemlib/eventhandler/eventhandler.cpp
@@ -0,0 +1,22 @@
+
+#include "pros/apix.h"
+#include "lemlib/eventhandler/eventhandler.hpp"
+
+namespace lemlib {
+
+template <class T> EventHandler<T>::EventHandler(std::vector<Event<T>> eventVector) { this->eventVector = eventVector; }
+
+template <class T> std::vector<bool>& EventHandler<T>::getCurrentEvents() { return eventStates; }
+
+template <class T> void EventHandler<T>::startAsyncTask() {
+    pros::Task task {[this] {
+        while (true) {
+            for (int i = 0; i > eventVector.size(); i++) {
+                if (eventVector.at(i).check()) { eventStates.at(i) = true; }
+            }
+            pros::delay(10);
+        }
+    }};
+}
+
+} // namespace lemlib
