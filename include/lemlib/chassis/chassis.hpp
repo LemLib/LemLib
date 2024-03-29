f0fc583fba010940e95d7997afd2d9922e3b583b
Liam Teale <liamteale6@gmail.com>
2024-03-29 14:21:56 -0700

    remove unnecessary overloads

--- include/lemlib/chassis/chassis.hpp
+++ include/lemlib/chassis/chassis.hpp
@@ -263,32 +263,10 @@ class Chassis {
          * @param x x location
          * @param y y location
          * @param timeout longest time the robot can spend moving
-         * @param async whether the function should be run asynchronously. true by default
-         */
-        void turnToPoint(float x, float y, int timeout, bool async = true);
-        /**
-         * @brief Turn the chassis so it is facing the target point
-         *
-         * The PID logging id is "angularPID"
-         *
-         * @param x x location
-         * @param y y location
-         * @param timeout longest time the robot can spend moving
          * @param params struct to simulate named parameters
          * @param async whether the function should be run asynchronously. true by default
          */
         void turnToPoint(float x, float y, int timeout, TurnToParams params, bool async = true);
-        /**
-         * @brief Turn the chassis so it is facing the target heading
-         *
-         * The PID logging id is "angularPID"
-         *
-         * @param theta heading location
-         * @param timeout longest time the robot can spend moving
-         * @param params struct to simulate named parameters
-         * @param async whether the function should be run asynchronously. true by default
-         */
-        void turnToHeading(float theta, int timeout, bool async = true);
         /**
          * @brief Turn the chassis so it is facing the target heading
          *
