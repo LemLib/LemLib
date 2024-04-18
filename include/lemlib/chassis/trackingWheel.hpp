abd24513d159226ed6239854e5988f7272c48ac8
RhettB1214 <102620185+RhettB1214@users.noreply.github.com>
2024-04-17 23:25:45 -0400

    Added 2" Omni

--- include/lemlib/chassis/trackingWheel.hpp
+++ include/lemlib/chassis/trackingWheel.hpp
@@ -10,6 +10,7 @@ namespace lemlib {
  * @brief A namespace representing the size of omniwheels.
  */
 namespace Omniwheel {
+constexpr float NEW_2 = 2.125;
 constexpr float NEW_275 = 2.75;
 constexpr float OLD_275 = 2.75;
 constexpr float NEW_275_HALF = 2.744;
