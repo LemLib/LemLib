4eb9d3cfca872cdb5f402b2e1e1d524b73fe0463
Liam Teale <111480281+SizzinSeal@users.noreply.github.com>
2024-04-21 19:00:31 -0700

    add example code to `Chassis::turnToHeading()`

--- include/lemlib/chassis/chassis.hpp
+++ include/lemlib/chassis/chassis.hpp
@@ -541,6 +541,27 @@ class Chassis {
          * @param timeout longest time the robot can spend moving
          * @param params struct to simulate named parameters
          * @param async whether the function should be run asynchronously. true by default
+         *
+         * @example
+         * @code {.cpp}
+         * void autonomous() {
+         *    chassis.setPose(0, 0, 0); // set the pose of the chassis to x = 0, y = 0, theta = 0
+         *    // turn the robot to face heading 135, with a timeout of 1000ms
+         *    chassis.turnToPoint(135, 1000);
+         *    // turn the robot to face heading 230.5 with a timeout of 2000ms
+         *    // and a maximum speed of 60
+         *    chassis.turnToPoint(230.5, 2000, {.maxSpeed = 60});
+         *    // turn the robot to face heading -90 with a timeout of 1500ms
+         *    // and turn counterclockwise
+         *    chassis.turnToPoint(-90, 1500, {.direction = AngularDirection::CCW_COUNTERCLOCKWISE});
+         *    // turn the robot to face heading 90 with a timeout of 500ms
+         *    // with a minSpeed of 20 and a maxSpeed of 60
+         *    chassis.turnToPoint(90, 500, {.minSpeed = 20, .maxSpeed = 60});
+         *    // turn the robot to face heading 45 with a timeout of 2000ms
+         *    // and a minSpeed of 60, and exit the movement if the robot is within 5 degrees of the target
+         *    chassis.turnToPoint(45, 2000, {.minSpeed = 60, .earlyExitRange = 5});
+         }
+         * @endcode
          */
         void turnToHeading(float theta, int timeout, TurnToHeadingParams params = {}, bool async = true);
         /**
