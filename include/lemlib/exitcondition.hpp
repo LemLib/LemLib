a196c1c1ff97bcaf266d2a16304b60acb222f979
Andrew Curtis <80860310+meisZWFLZ@users.noreply.github.com>
2024-04-23 17:25:27 +0000

    docs: :bulb: document exit conditions in header

--- include/lemlib/exitcondition.hpp
+++ include/lemlib/exitcondition.hpp
@@ -5,6 +5,9 @@
 #include <vector>
 
 namespace lemlib {
+/**
+ * @brief Abstract exit condition. Intended for use only by advanced users / internally.
+ */
 class ExitCondition {
     public:
         /**
@@ -24,8 +27,19 @@ class ExitCondition {
         virtual bool update(const float input) = 0;
 };
 
+/**
+ * @brief Exit condition that exits when input has been in `range` for longer than `time` ms
+ *
+ * @warning Error exit conditions are only intended to be used internally and by advanced users.
+ * Most of the time the user should only need to interact with `ErrorExitCondition::Config`
+ */
 class ErrorExitCondition : public ExitCondition {
     public:
+        /**
+         * @brief Configuration for an `ErrorExitCondition`
+         *
+         * @see ErrorExitCondition
+         */
         struct Config {
                 /** @brief the range where the countdown is allowed to start */
                 float range;
@@ -68,7 +82,8 @@ class ErrorExitCondition : public ExitCondition {
 };
 
 /**
- * @brief exits if any of the children exit
+ * @brief Exits if any of it's children are currently in an exitted state. Intended for use only by advanced users /
+ * internally.
  */
 class ExitConditionGroup : public ExitCondition {
     public:
@@ -97,14 +112,24 @@ class ExitConditionGroup : public ExitCondition {
         std::vector<std::shared_ptr<ExitCondition>> children;
 };
 
+/**
+ * @brief Abstract factory for creating an `ExitCondition`. Intended for use only by advanced users / internally.
+ */
 class ExitConditionFactory {
     public:
         virtual std::unique_ptr<ExitCondition> create() const = 0;
 };
 
+/**
+ * @brief Used by user to configure multiple ErrorExitConditions whilst ensuring atomicity / thread-safety.
+ * Used by the chassis to create exit conditions for motions.
+ *
+ * Intended for use by slightly advanced users
+ */
 class ErrorExitConditionGroupFactory : public ExitConditionFactory {
     public:
         ErrorExitConditionGroupFactory(std::vector<ErrorExitCondition::Config> configs);
+
         /** @brief removes all configs */
         void clear();
 
@@ -114,10 +139,15 @@ class ErrorExitConditionGroupFactory : public ExitConditionFactory {
         /** @brief creates a new ExitConditionGroup with the current configs */
         std::unique_ptr<ExitCondition> create() const override;
 
+        /** @brief modifies the configs whilst ensuring atomicity */
         void setConfigs(std::vector<ErrorExitCondition::Config>& newConfig);
+
+        /** @brief reads the configs whilst ensuring atomicity */
         std::vector<ErrorExitCondition::Config> getConfigs() const;
     private:
+        /** @brief used to ensure atomicity */
         mutable pros::Mutex mutex;
+        /** @brief used to create ErrorExitConditions by `ErrorExitConditionGroupFactory::create()` */
         std::vector<ErrorExitCondition::Config> configs;
 };
 } // namespace lemlib
