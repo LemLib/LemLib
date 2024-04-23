c9f9b8be008b2cc3305d262da3c66278b981d16a
meisZWFLZ <atcurtis08@gmail.com>
2024-04-22 19:03:12 -0700

    feat: :sparkles: pid gain updating post-construction

--- include/lemlib/pid.hpp
+++ include/lemlib/pid.hpp
@@ -1,8 +1,38 @@
 #pragma once
 
+#include "pros/rtos.hpp"
+#include <atomic>
+#include <optional>
+
 namespace lemlib {
 class PID {
     public:
+        struct Gains {
+                /** @brief integral gain */
+                float kP;
+                /** @brief proportional gain */
+                float kI;
+                /** @brief derivative gain */
+                float kD;
+                /** @brief integral anti windup range */
+                float windupRange = 0;
+                /** @brief whether to reset integral when sign of error flips */
+                bool signFlipReset = false;
+        };
+
+        struct OptionalGains {
+                /** @brief integral gain */
+                std::optional<float> kP;
+                /** @brief proportional gain */
+                std::optional<float> kI;
+                /** @brief derivative gain */
+                std::optional<float> kD;
+                /** @brief integral anti windup range */
+                std::optional<float> windupRange = 0;
+                /** @brief whether to reset integral when sign of error flips */
+                std::optional<bool> signFlipReset = false;
+        };
+
         /**
          * @brief Construct a new PID
          *
@@ -13,6 +43,16 @@ class PID {
          * @param signFlipReset whether to reset integral when sign of error flips
          */
         PID(float kP, float kI, float kD, float windupRange = 0, bool signFlipReset = false);
+        /**
+         * @brief Construct a new PID
+         *
+         * @param kP proportional gain
+         * @param kI integral gain
+         * @param kD derivative gain
+         * @param windupRange integral anti windup range
+         * @param signFlipReset whether to reset integral when sign of error flips
+         */
+        PID(const PID::Gains& gains);
 
         /**
          * @brief Update the PID
@@ -24,18 +64,25 @@ class PID {
 
         /**
          * @brief reset integral, derivative, and prevTime
-         *
          */
         void reset();
-    protected:
-        // gains
-        const float kP;
-        const float kI;
-        const float kD;
 
-        // optimizations
-        const float windupRange;
-        const bool signFlipReset;
+        /**
+         * @brief update the gains object while only updating the gains that are specified and ensuring atomicity
+         */
+        void setGains(const PID::OptionalGains& gains);
+        /**
+         * @brief update the gains object while and ensuring atomicity
+         */
+        void setGains(const PID::Gains& gains);
+        /**
+         * @brief get the gains object while ensuring atomicity
+         */
+        PID::Gains getGains() const;
+    protected:
+        PID::Gains gains;
+        /** used to ensure the gains' atomicity */
+        mutable pros::Mutex gainsMutex;
 
         float integral = 0;
         float prevError = 0;
--- src/lemlib/pid.cpp
+++ src/lemlib/pid.cpp
@@ -1,5 +1,6 @@
 #include "pid.hpp"
 #include "util.hpp"
+#include <mutex>
 
 namespace lemlib {
 /**
@@ -12,11 +13,10 @@ namespace lemlib {
  * @param signFlipReset whether to reset integral when sign of error flips
  */
 PID::PID(float kP, float kI, float kD, float windupRange, bool signFlipReset)
-    : kP(kP),
-      kI(kI),
-      kD(kD),
-      windupRange(windupRange),
-      signFlipReset(signFlipReset) {}
+    : PID(PID::Gains {.kP = kP, .kI = kI, .kD = kD, .windupRange = windupRange, .signFlipReset = signFlipReset}) {}
+
+PID::PID(const PID::Gains& gains)
+    : gains(gains) {}
 
 /**
  * @brief Update the PID
@@ -25,17 +25,37 @@ PID::PID(float kP, float kI, float kD, float windupRange, bool signFlipReset)
  * @return float output
  */
 float PID::update(const float error) {
+    const PID::Gains gains = this->getGains();
     // calculate integral
     integral += error;
-    if (sgn(error) != sgn((prevError)) && signFlipReset) integral = 0;
-    if (fabs(error) > windupRange && windupRange != 0) integral = 0;
+    if (sgn(error) != sgn((prevError)) && gains.signFlipReset) integral = 0;
+    if (fabs(error) > gains.windupRange && gains.windupRange != 0) integral = 0;
 
     // calculate derivative
     const float derivative = error - prevError;
     prevError = error;
 
     // calculate output
-    return error * kP + integral * kI + derivative * kD;
+    return error * gains.kP + integral * gains.kI + derivative * gains.kD;
+}
+
+void PID::setGains(const PID::OptionalGains& newGains) {
+    const PID::Gains oldGains = this->getGains();
+    this->setGains(PID::Gains {.kP = newGains.kP.value_or(oldGains.kP),
+                               .kI = newGains.kI.value_or(oldGains.kI),
+                               .kD = newGains.kD.value_or(oldGains.kD),
+                               .windupRange = newGains.windupRange.value_or(oldGains.windupRange),
+                               .signFlipReset = newGains.signFlipReset.value_or(oldGains.signFlipReset)});
+}
+
+void PID::setGains(const PID::Gains& newGains) {
+    std::lock_guard guard(this->gainsMutex);
+    this->gains = newGains;
+}
+
+PID::Gains PID::getGains() const {
+    std::lock_guard guard(this->gainsMutex);
+    return this->gains;
 }
 
 /**
