8d14111780eb1c6b5515cff584b94364a537a4d9
Liam Teale <liamteale6@gmail.com>
2024-05-20 20:18:44 -0700

    document abstract motion

--- include/lemlib/motions/motion.hpp
+++ include/lemlib/motions/motion.hpp
@@ -1,32 +1,101 @@
 #include "lemlib/pose.hpp"
 
 namespace lemlib {
-enum class MotionState { RUNNING, FINISHED };
+/**
+ * @enum MotionState
+ * 
+ * @brief Enum to represent the state of a motion
+ */
+enum class MotionState {
+    RUNNING, /** the motion is currently running */
+    FINISHED /** the motion is finished */
+};
 
+/**
+ * @struct DifferentialChassisSpeeds1
+ * 
+ * @brief represents the speed of the left and right wheels of a differential drive robot
+ */
 struct DifferentialChassisSpeeds {
-        float L, R;
+        float left; /** the speed of the left wheels */
+        float right; /** the speed of the right wheels */
 };
 
+/**
+ * @struct HolonomicChassisSpeeds
+ * 
+ * @brief represents the speed of the x, y, and angular velocity of a holonomic drive robot
+ */
 struct HolonomicChassisSpeeds {
-        float X, Y, Omega;
+        float x; /** x velocity */
+        float y; /** y velocity */
+        float omega; /** angular velocity */
 };
 
+/**
+ * @class DifferentialMotion
+ * 
+ * @brief Abstract class which represents a motion algorithm for a differential drive robot
+ *
+ * We use this abstraction to allow for different motion algorithms to be used with the same interface.
+ * This allows us to easily create and maintain motion algorithms while also allowing the user to
+ * make custom motion algorithms without having to recompile the library.
+ */
 class DifferentialMotion {
     public:
+        /**
+         * @brief Calculates the speed of the left and right wheels of a differential drive robot
+         * 
+         * @param pose the current pose of the robot
+         * @return DifferentialChassisSpeeds the speed of the left and right wheels
+         */
         virtual DifferentialChassisSpeeds calculate(Pose pose) = 0;
+        /**
+         * @brief Get the state of the motion
+         * 
+         * @return MotionState the state of the motion
+         */
         MotionState getState();
+        /**
+         * @brief Destroy the Differential Motion object
+         * 
+         */
         virtual ~DifferentialMotion();
     protected:
-        MotionState state = MotionState::RUNNING;
+        MotionState state = MotionState::RUNNING; /** the state of the motion */
 };
 
+/**
+ * @class HolonomicMotion
+ * 
+ * @brief Abstract class which represents a motion algorithm for a holonomic drive robot
+ *
+ * We use this abstraction to allow for different motion algorithms to be used with the same interface.
+ * This allows us to easily create and maintain motion algorithms while also allowing the user to
+ * make custom motion algorithms without having to recompile the library.
+ */
 class HolonomicMotion {
     public:
+        /**
+         * @brief Calculates the field-relative speed of a holonomic drive robot
+         * 
+         * @param pose the current pose of the robot
+         * @return HolonomicChassisSpeeds the field-relative speed of the robot
+         */
         virtual HolonomicChassisSpeeds calculate(Pose pose) = 0;
+        /**
+         * @brief Get the state of the motion
+         * 
+         * @return MotionState the state of the motion
+         */
         MotionState getState();
+        /**
+         * @brief Destroy the Holonomic Motion object
+         * 
+         */
         virtual ~HolonomicMotion();
     protected:
-        MotionState state = MotionState::RUNNING;
+        MotionState state = MotionState::RUNNING; /** the state of the motion */
 };
 
 } // namespace lemlib
