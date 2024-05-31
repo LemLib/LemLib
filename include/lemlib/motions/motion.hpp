43b9adb4cc736937c18f6cdc1ec22ae010203184
Liam Teale <liamteale6@gmail.com>
2024-05-31 09:55:46 -0700

    document DifferentialMotion ctor

--- include/lemlib/motions/motion.hpp
+++ include/lemlib/motions/motion.hpp
@@ -37,6 +37,17 @@ struct HolonomicChassisSpeeds {
  */
 class DifferentialMotion {
     public:
+        /**
+         * @brief Construct a new Differential Motion object
+         *
+         * The parameters are used to calculate the linear and angular velocity of the robot
+         * as well as to modify the output speeds to prevent the motors from becoming saturated
+         *
+         * @param trackWidth the track width of the robot
+         * @param maxVelocity the maximum theoretical velocity of the robot
+         * @param desaturateBias the weight to determine whether to prioritize linear or angular velocity. 0 fully
+         * prioritizes linear velocity, 1 fully prioritizes angular velocity. Default is 0.5
+         */
         DifferentialMotion(const Length trackWidth, const LinearVelocity maxVelocity, const float desaturateBias = 0.5);
         /**
          * @brief Calculates the speed of the left and right wheels of a differential drive robot
