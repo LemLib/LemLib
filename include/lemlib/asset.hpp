eaa4dd89f1d7f6b982a75a3453885f71cb1b6921
Liam Teale <111480281+SizzinSeal@users.noreply.github.com>
2024-08-09 16:11:08 -0700

    fix formatting

--- include/lemlib/asset.hpp
+++ include/lemlib/asset.hpp
@@ -8,24 +8,21 @@
 extern "C" {
 
 typedef struct __attribute__((__packed__)) _asset {
-  uint8_t *buf;
-  size_t size;
+        uint8_t* buf;
+        size_t size;
 } asset;
 }
 
-#define ASSET(x)                                                               \
-  extern "C" {                                                                 \
-  extern uint8_t _binary_static_##x##_start[], _binary_static_##x##_size[];    \
-  static asset x = {_binary_static_##x##_start,                                \
-                    (size_t)_binary_static_##x##_size};                        \
-  }
+#define ASSET(x)                                                                                                       \
+    extern "C" {                                                                                                       \
+    extern uint8_t _binary_static_##x##_start[], _binary_static_##x##_size[];                                          \
+    static asset x = {_binary_static_##x##_start, (size_t)_binary_static_##x##_size};                                  \
+    }
 
-#define ASSET_LIB(x)                                                           \
-  extern "C" {                                                                 \
-  extern uint8_t _binary_static_lib_##x##_start[],                             \
-      _binary_static_lib_##x##_size[];                                         \
-  static asset x = {_binary_static_lib_##x##_start,                            \
-                    (size_t)_binary_static_lib_##x##_size};                    \
-  }
+#define ASSET_LIB(x)                                                                                                   \
+    extern "C" {                                                                                                       \
+    extern uint8_t _binary_static_lib_##x##_start[], _binary_static_lib_##x##_size[];                                  \
+    static asset x = {_binary_static_lib_##x##_start, (size_t)_binary_static_lib_##x##_size};                          \
+    }
 
 #endif // _ASSET_H_
