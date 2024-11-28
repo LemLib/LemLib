f2f60330e4d1772cebaea84b349a9cec3df0d287
Liam Teale <liamteale6@gmail.com>
2024-11-28 11:26:25 -0800

    don't include macros if LEMLIB_NO_MACROS is defined

--- include/lemlib/lemlib.hpp
+++ include/lemlib/lemlib.hpp
@@ -1,4 +1,7 @@
 #pragma once
 
+#ifndef LEMLIB_NO_MACROS
+#include "lemlib/macros.hpp" // IWYU pragma: keep
+#endif
+
 #include "lemlib/MotionHandler.hpp" // IWYU pragma: keep
-#include "lemlib/util.hpp" // IWYU pragma: keep
