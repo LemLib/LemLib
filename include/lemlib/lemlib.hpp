62c6cb952681aa82edb836f62f465d4c0aceb3ee
Liam Teale <liamteale6@gmail.com>
2024-11-28 18:03:03 -0800

    add shorthand namespaces

--- include/lemlib/lemlib.hpp
+++ include/lemlib/lemlib.hpp
@@ -1,7 +1,12 @@
 #pragma once
 
 #ifndef LEMLIB_NO_MACROS
-#include "lemlib/macros.hpp" // IWYU pragma: keep
+#include "lemlib/macros.hpp"
 #endif
 
-#include "lemlib/MotionHandler.hpp" // IWYU pragma: keep
+#include "lemlib/MotionHandler.hpp"
+
+#ifndef LEMLIB_NO_ALIAS
+namespace ll = lemlib;
+namespace mh = lemlib::MotionHandler;
+#endif
