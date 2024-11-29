73d8fe139de6dc7ecbcc34c510e9e49a4a3bf7d9
Liam Teale <liamteale6@gmail.com>
2024-11-28 19:50:21 -0800

    properly handle scheduler lag in MotionCancelHelper

--- src/lemlib/MotionCancelHelper.cpp
+++ src/lemlib/MotionCancelHelper.cpp
@@ -8,8 +8,14 @@ MotionCancelHelper::MotionCancelHelper()
       prevTime(pros::millis()) {}
 
 bool MotionCancelHelper::wait(Time timeout) {
+    const std::uint32_t processedTimeout = to_msec(timeout);
+    // if current time - previous time > timeout
+    // then set previous time to current time
+    // this is to prevent the motion iterating multiple times
+    const int64_t now = int64_t(pros::millis());
+    if (now - int64_t(prevTime) > int64_t(processedTimeout)) prevTime = now - processedTimeout;
     // only delay if this is not the first iteration
-    if (!firstIteration) pros::Task::delay_until(&prevTime, to_msec(timeout));
+    if (!firstIteration) pros::Task::delay_until(&prevTime, processedTimeout);
     else firstIteration = false;
 
     // if the competition state is not the same as when the motion started, then stop the motion
