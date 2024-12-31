e0a515c3e8e211ab5d99c23d141d5676404d8445
Exortions <exortionsmc@gmail.com>
2024-12-30 20:10:37 -0800

    start turnToHeading

--- include/lemlib/motions/turnToHeading.hpp
+++ include/lemlib/motions/turnToHeading.hpp
@@ -1,1 +1,27 @@
 #pragma once
+
+#include "lemlib/ExitCondition.hpp"
+#include "lemlib/util.hpp"
+#include "units/Angle.hpp"
+#include "units/units.hpp"
+#include "units/Pose.hpp"
+#include "lemlib/pid.hpp"
+#include <functional>
+
+namespace lemlib {
+struct TurnToHeadingParams {
+        AngularDirection direction = AngularDirection::AUTO;
+        int maxSpeed = 127;
+        int minSpeed = 0;
+        Angle earlyExitRange = 0_cRot;
+};
+
+struct TurnToHeadingSettings {
+        PID angularPID;
+        ExitCondition angularLargeExit;
+        ExitCondition angularSmallExit;
+        std::function<units::Pose()> poseGetter;
+};
+
+void turnToHeading(Angle heading, Time timeout, TurnToHeadingParams params, TurnToHeadingSettings settings);
+} // namespace lemlib
---
+++ src/lemlib/motions/turnToHeading.cpp
@@ -0,0 +1,77 @@
+#include "lemlib/motions/turnToHeading.hpp"
+#include "pros/apix.h"
+#include <optional>
+
+void lemlib::turnToHeading(Angle heading, Time timeout, lemlib::TurnToHeadingParams params,
+                           lemlib::TurnToHeadingSettings settings) {
+    params.minSpeed = abs(params.minSpeed);
+
+    // TODO: Motion handler stuff
+    // TODO: Timer
+
+    settings.angularLargeExit.reset();
+    settings.angularSmallExit.reset();
+    settings.angularPID.reset();
+
+    std::optional<Angle> previousRawDeltaTheta = std::nullopt;
+    std::optional<Angle> previousDeltaTheta = std::nullopt;
+    Angle startingTheta = settings.poseGetter().theta();
+    Angle angleTraveled = 0_stRot;
+    Angle targetTheta = heading;
+    Angle deltaTheta = 0_stRot;
+
+    bool settling = false;
+
+    double previousMotorPower = 0.0;
+    double motorPower = 0.0;
+
+    while (!settings.angularLargeExit.update(deltaTheta.convert(deg)) &&
+           !settings.angularSmallExit.update(deltaTheta.convert(deg))) {
+        // get the robot's current position
+        units::Pose pose = settings.poseGetter();
+
+        // update angle traveled
+        angleTraveled = units::abs(lemlib::angleError(pose.theta(), startingTheta));
+
+        targetTheta = heading;
+
+        // check if settling
+        const Angle rawDeltaTheta = angleError(targetTheta, pose.theta());
+        if (previousRawDeltaTheta == std::nullopt) previousRawDeltaTheta = rawDeltaTheta;
+        if (units::sgn(rawDeltaTheta) != units::sgn(previousRawDeltaTheta.value())) settling = true;
+        previousRawDeltaTheta = rawDeltaTheta;
+
+        // calculate delta theta
+        if (settling) deltaTheta = angleError(targetTheta, pose.theta());
+        else deltaTheta = angleError(targetTheta, pose.theta(), params.direction);
+        if (previousDeltaTheta == std::nullopt) previousDeltaTheta = deltaTheta;
+
+        // motion chaining
+        // exit the motion to immediately continue to the next one
+        if (params.minSpeed != 0 && units::abs(deltaTheta) < params.earlyExitRange) break;
+        if (params.minSpeed != 0 && units::sgn(deltaTheta) != units::sgn(previousDeltaTheta.value())) break;
+
+        // calculate speed
+        motorPower = settings.angularPID.update(deltaTheta.convert(deg));
+
+        if (motorPower > params.maxSpeed) motorPower = params.maxSpeed;
+        else if (motorPower < -params.maxSpeed) motorPower = -params.maxSpeed;
+        if (units::abs(deltaTheta) > 20_stDeg)
+            motorPower = lemlib::slew(motorPower, previousMotorPower, settings.angularPID.getGains().slew);
+        if (motorPower < 0 && motorPower > -params.minSpeed) motorPower = -params.minSpeed;
+        else if (motorPower > 0 && motorPower < params.minSpeed) motorPower = params.minSpeed;
+        previousMotorPower = motorPower;
+
+        // move the motors
+        // TODO: add drivetrain
+
+        pros::delay(10);
+    }
+
+    // stop the drivetrain
+    // TODO: add drivetrain
+
+    // TODO: do stuff with angle traveled
+
+    // TODO: end the motion
+}
