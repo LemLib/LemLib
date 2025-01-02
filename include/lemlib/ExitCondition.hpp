ec43e6a6ddacafe398e0e021a91f20013b28ddd6
Exortions <exortionsmc@gmail.com>
2025-01-02 14:57:06 -0800

    template ExitCondition & ExitConditionGroup

--- include/lemlib/ExitCondition.hpp
+++ include/lemlib/ExitCondition.hpp
@@ -1,11 +1,12 @@
 #pragma once
 
 #include "units/units.hpp"
+#include "pros/rtos.hpp"
 #include <optional>
 #include <vector>
 
 namespace lemlib {
-class ExitCondition {
+template <isQuantity Q> class ExitCondition {
     public:
         /**
          * @brief Create a new exit condition
@@ -18,7 +19,11 @@ class ExitCondition {
          * // create a new exit condition that requires the input to be within 250 of 0 for 200 milliseconds
          * lemlib::ExitCondition exitCondition(250, 200_msec);
          */
-        ExitCondition(double range, Time time);
+        ExitCondition(Q range, Time time)
+            : m_startTime(std::nullopt),
+              m_range(range),
+              m_time(time) {}
+
         /**
          * @brief Update the exit condition
          *
@@ -34,7 +39,15 @@ class ExitCondition {
          *   doSomething();
          * }
          */
-        bool update(double input);
+        bool update(Q input) {
+            const Time currentTime = pros::millis() * msec;
+            if (fabs(input) >= m_range) m_startTime.reset();
+            else if (m_startTime == -1 * sec) m_startTime = currentTime;
+            else if (currentTime >= m_startTime.value() + m_time) m_done = true;
+
+            return m_done;
+        }
+
         /**
          * @brief Resets the exit condition (the timer and the done flag)
          *
@@ -43,22 +56,27 @@ class ExitCondition {
          * // reset the exit condition
          * exitCondition.reset();
          */
-        void reset();
+        void reset() {
+            m_startTime.reset();
+            m_done = false;
+        }
     private:
         std::optional<Time> m_startTime;
         bool m_done = false;
-        double m_range;
+        Q m_range;
         Time m_time;
 };
 
-class ExitConditionGroup {
+template <isQuantity Q> class ExitConditionGroup {
     public:
         /**
          * @brief Create a new exit condition group
          *
          * @param exitConditions the exit conditions to check
          */
-        ExitConditionGroup(std::vector<ExitCondition> exitConditions);
+        ExitConditionGroup(std::vector<ExitCondition<Q>> exitConditions)
+            : m_exitConditions(exitConditions) {}
+
         /**
          * @brief Update the exit condition group
          *
@@ -66,31 +84,42 @@ class ExitConditionGroup {
          * @return true
          * @return false
          */
-        bool update(double input);
+        bool update(Q input) {
+            for (auto& exitCondition : m_exitConditions) {
+                if (exitCondition.update(input)) return true;
+            }
+            return false;
+        }
+
         /**
          * @brief Resets the exit condition group
          *
          */
-        void reset();
+        void reset() {
+            for (auto& exitCondition : m_exitConditions) { exitCondition.reset(); }
+        }
+
         /**
          * @brief Set the exit conditions in the group
          *
          * @param exitConditions the new list of exit conditions
          */
-        void setExitConditions(std::vector<ExitCondition> exitConditions);
+        void setExitConditions(std::vector<ExitCondition<Q>> exitConditions) { m_exitConditions = exitConditions; }
+
         /**
          * @brief Get the exit conditions in the group
          *
          * @return std::vector<ExitCondition> the exit conditions
          */
-        std::vector<ExitCondition> getExitConditions();
+        std::vector<ExitCondition<Q>> getExitConditions() { return m_exitConditions; }
+
         /**
          * @brief Add an exit condition to the group
          *
          * @param exitCondition the exit condition to add
          */
-        void addExitCondition(ExitCondition exitCondition);
+        void addExitCondition(ExitCondition<Q> exitCondition) { m_exitConditions.push_back(exitCondition); }
     private:
-        std::vector<ExitCondition> m_exitConditions;
+        std::vector<ExitCondition<Q>> m_exitConditions;
 };
 } // namespace lemlib
