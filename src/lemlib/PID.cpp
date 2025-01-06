efb28b191e260e40dc60c3dc4dc854f5274e3980
Liam Teale <liamteale6@gmail.com>
2025-01-05 16:32:57 -0800

    fix PID derivative handling

--- src/lemlib/PID.cpp
+++ src/lemlib/PID.cpp
@@ -2,30 +2,42 @@
 #include "lemlib/PID.hpp"
 #include "pros/rtos.hpp"
 
-lemlib::PID::PID(double kP, double kI, double kD, double windupRange, bool signFlipReset)
+namespace lemlib {
+
+PID::PID(double kP, double kI, double kD, double windupRange, bool signFlipReset)
     : m_gains({kP, kI, kD}),
       m_windupRange(windupRange),
       m_signFlipReset(signFlipReset) {}
 
-lemlib::PID::PID(const lemlib::Gains& gains, double windupRange, bool signFlipReset)
+PID::PID(const Gains& gains, double windupRange, bool signFlipReset)
     : m_gains(gains),
       m_windupRange(windupRange),
       m_signFlipReset(signFlipReset) {}
 
-lemlib::Gains lemlib::PID::getGains() { return m_gains; }
+Gains PID::getGains() { return m_gains; }
 
-void lemlib::PID::setGains(lemlib::Gains gains) { m_gains = gains; }
+void PID::setGains(lemlib::Gains gains) { m_gains = gains; }
 
-double lemlib::PID::update(double error) {
-    Time dt = m_previousTime == 0_sec ? 0_msec : (pros::millis() * msec) - m_previousTime;
+double PID::update(double error) {
+    // find time delta
+    const Time now = from_msec(pros::millis());
+    // if this is the first iteration, previousTime won't be set
+    // if it is not set, then assume dt is 0
+    Time dt = (m_previousTime == std::nullopt) ? 0_msec : now - *m_previousTime;
+    m_previousTime = now;
 
-    m_integral += error * dt.convert(sec);
-    if (lemlib::sgn(error) != lemlib::sgn((m_previousError)) && m_signFlipReset) m_integral = 0;
-    if (fabs(error) > m_windupRange && m_windupRange != 0) m_integral = 0;
-
-    const double derivative = (error - m_previousError) / dt.convert(sec);
+    // calculate the derivative (change in error / time passed)
+    const double derivative = (dt != 0_sec) ? (error - m_previousError) / to_sec(dt) : 0;
     m_previousError = error;
 
+    // calculate the integral (change in error * time passed)
+    m_integral += error * to_sec(dt);
+    // sign flip reset. If the sign of error changes, set the integral to 0
+    if (sgn(error) != sgn((m_previousError)) && m_signFlipReset) m_integral = 0;
+    // anti windup range. Unless error is small enough, set the integral to 0
+    if (fabs(error) > m_windupRange && m_windupRange != 0) m_integral = 0;
+
+    // output. error * kP + integral * kP + derivative * kD
     return error * m_gains.kP + m_integral * m_gains.kI + derivative * m_gains.kD;
 }
 
@@ -41,3 +53,5 @@ bool lemlib::PID::getSignFlipReset() { return m_signFlipReset; }
 void lemlib::PID::setWindupRange(double windupRange) { m_windupRange = windupRange; }
 
 double lemlib::PID::getWindupRange() { return m_windupRange; }
+
+} // namespace lemlib
