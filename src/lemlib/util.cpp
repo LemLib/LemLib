89cdaaae89339488f412feeefdde54f93d5c0577
Liam Teale <liamteale6@gmail.com>
2025-01-05 19:12:57 -0800

    util improvements part 1

--- src/lemlib/util.cpp
+++ src/lemlib/util.cpp
@@ -1,37 +1,39 @@
 #include "lemlib/util.hpp"
 
-Angle lemlib::angleError(Angle target, Angle position, lemlib::AngularDirection direction) {
+namespace lemlib {
+
+Angle angleError(Angle target, Angle position, AngularDirection direction) {
     // Wrap the angle to be within 0pi and 2pi radians
     target = units::mod(units::mod(target, 1_stRot) + 1_stRot, 1_stRot);
 
     Angle error = target - position;
 
     switch (direction) {
-        case lemlib::AngularDirection::CW_CLOCKWISE: return error < 0_stRot ? error + 1_stRot : error;
-        case lemlib::AngularDirection::CCW_COUNTERCLOCKWISE: return error > 0_stRot ? error - 1_stRot : error;
-        default: return std::remainder(error.convert(deg), 360) * deg; // units does not have a remainder function
+        case AngularDirection::CW_CLOCKWISE: return error < 0_stRot ? error + 1_stRot : error;
+        case AngularDirection::CCW_COUNTERCLOCKWISE: return error > 0_stRot ? error - 1_stRot : error;
+        default: return from_stDeg(std::remainder(to_stDeg(error), 360)); // units does not have a remainder function
     }
 }
 
-double lemlib::slew(double target, double current, double maxChange) {
-    double change = target - current;
-    if (maxChange == 0) return target;
-    if (change > maxChange) change = maxChange;
-    else if (change < -maxChange) change = -maxChange;
-    return current + change;
+Number slew(Number target, Number current, Number maxChange, Time deltaTime, SlewDirection restrictDirection) {
+    const Number change = target - current;
+
+    // only restrict change for specified directions
+    if (restrictDirection == SlewDirection::INCREASING && change < 0) return target;
+    if (restrictDirection == SlewDirection::DECREASING && change > 0) return target;
+
+    // check if the change is within the limit
+    if (units::abs(change) > units::abs(maxChange)) return current + (maxChange * units::sgn(change));
+
+    // return the target if no restriction is necessary
+    return target;
 }
 
-double lemlib::respectSpeeds(double power, double previous, double max, double min, double slew) {
-    // respect maximum speed
-    if (power > max) power = max;
-    else if (power < -max) power = -max;
-
-    // optionally slew if enabled
-    if (slew != 0) power = lemlib::slew(power, previous, slew);
-
+Number respectSpeeds(Number power, Number max, Number min) {
     // respect minimum speed
-    if (power < 0 && power > -min) power = -min;
-    else if (power > 0 && power < min) power = min;
-
+    if (units::abs(power) < min) power = units::sgn(power) * min;
+    // respect maximum speed
+    power = std::clamp(power, -max, max);
     return power;
 }
+} // namespace lemlib
