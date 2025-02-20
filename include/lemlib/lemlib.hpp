b60ed3b2d4594cd181e0cc9c3c81b7bd78f66d14
Liam Teale <liamteale6@gmail.com>
2025-02-19 23:35:36 -0800

    update lemlib.hpp

--- include/lemlib/lemlib.hpp
+++ include/lemlib/lemlib.hpp
@@ -1,13 +1,15 @@
 #pragma once
 
 #ifndef LEMLIB_NO_MACROS
-#include "lemlib/macros.hpp"
+#include "lemlib/macros.hpp" // IWYU pragma: keep
 #endif
 
-#include "lemlib/tracking/TrackingWheelOdom.hpp"
-#include "lemlib/motions/turnToHeading.hpp"
-#include "lemlib/MotionHandler.hpp"
-#include "lemlib/PID.hpp"
+#include "lemlib/motions/follow.hpp" // IWYU pragma: keep
+#include "lemlib/motions/moveToPoint.hpp" // IWYU pragma: keep
+#include "lemlib/motions/moveToPose.hpp" // IWYU pragma: keep
+#include "lemlib/motions/turnTo.hpp" // IWYU pragma: keep
+#include "lemlib/tracking/TrackingWheelOdom.hpp" // IWYU pragma: keep
+#include "lemlib/MotionHandler.hpp" // IWYU pragma: keep
 
 #ifndef LEMLIB_NO_ALIAS
 namespace ll = lemlib;
