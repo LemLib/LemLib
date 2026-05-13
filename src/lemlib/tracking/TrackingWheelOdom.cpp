e6a872f55f9c5b46d2dc63101cde2e7d5d4af0d5
DESTROYERMJB <COOLNOODLEPERSON@GMAIL.COM>
2026-05-12 22:07:19 -0500

    Update TrackingWheelOdom.cpp

--- src/lemlib/tracking/TrackingWheelOdom.cpp
+++ src/lemlib/tracking/TrackingWheelOdom.cpp
@@ -40,7 +40,7 @@ TrackingWheel::TrackingWheel(SmartPort expanderPort, ADIPort topPort, ADIPort bo
       m_lastTotal(to_stRot(m_encoder->getAngle()) * M_PI * diameter * m_ratio),
       m_deallocate(true) {}
 
-Length TrackingWheel::getDistanceTraveled() { return to_stRot(m_encoder->getAngle()) * M_PI * m_diameter; }
+Length TrackingWheel::getDistanceTraveled() { return to_stRot(m_encoder->getAngle()) * M_PI * m_diameter * m_ratio; }
 
 Length TrackingWheel::getDistanceDelta() {
     // calculate delta
